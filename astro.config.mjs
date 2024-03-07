6a6ec838d733daa96e5ed5934792a75aa7ae6f6a
jelfes <janelfes@gmail.com>
2024-03-07 20:12:43 +0000

    add github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://astrofy-template.netlify.app',
+  site: 'https://jelfes.github.io',
+  // base: '/PersonalWebsite'
   integrations: [mdx(), sitemap(), tailwind()]
 });
